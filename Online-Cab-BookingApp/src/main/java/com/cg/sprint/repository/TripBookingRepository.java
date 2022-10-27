76fc7fb8204d313e66efee562697b91d377f13e4
mdmainuddin360 <115131966+mdmainuddin360@users.noreply.github.com>
2022-10-27 13:02:36 +0530

    repo and service class created for tripBooking

--- Online-Cab-BookingApp/src/main/java/com/cg/sprint/repository/TripBookingRepository.java
+++ Online-Cab-BookingApp/src/main/java/com/cg/sprint/repository/TripBookingRepository.java
@@ -1,5 +1,31 @@
 package com.cg.sprint.repository;
 
-public class TripBookingRepository {
+import java.util.List;
 
+import com.cg.sprint.entity.TripBooking;
+import com.cg.sprint.service.TripBookingService;
+
+public class TripBookingRepository {
+	
+	public TripBooking insertTripBooking(TripBooking tripBooking) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public TripBooking updateTripBooking(TripBookingService tripBooking) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public TripBooking deleteTripBooking(int tripBookingId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<TripBooking> viewAllTripCustomer(int customerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- Online-Cab-BookingApp/src/main/java/com/cg/sprint/service/impl/TripBookingServiceImpl.java
+++ Online-Cab-BookingApp/src/main/java/com/cg/sprint/service/impl/TripBookingServiceImpl.java
@@ -1,5 +1,40 @@
 package com.cg.sprint.service.impl;
 
-public class TripBookingServiceImpl {
+import java.util.List;
+
+import com.cg.sprint.entity.TripBooking;
+import com.cg.sprint.service.TripBookingService;
+
+public class TripBookingServiceImpl implements TripBookingService {
+
+	@Override
+	public TripBooking insertTripBooking(TripBooking tripBooking) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TripBooking updateTripBooking(TripBookingService tripBooking) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TripBooking deleteTripBooking(int tripBookingId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TripBooking> viewAllTripCustomer(int customerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TripBooking calculateBill(int customerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

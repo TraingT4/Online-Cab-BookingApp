998cf79563ada06c411b0623bcc17230516112ed
Dhikshitha <Dhikshitha@LAPTOP-NNDOLLH4>
2022-10-27 15:48:57 +0530

    changes committed

--- Online-Cab-BookingApp/src/main/java/com/cg/sprint/service/impl/CustomerServiceImpl.java
+++ Online-Cab-BookingApp/src/main/java/com/cg/sprint/service/impl/CustomerServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cg.sprint.entity.Customer;
 import com.cg.sprint.repository.CustomerRepository;
 import com.cg.sprint.service.CustomerService;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
